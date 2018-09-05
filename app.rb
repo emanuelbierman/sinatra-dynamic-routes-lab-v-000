45793b368eb478f0d0154630912c5266cac079ea
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-05 17:56:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -42,7 +42,10 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    
+    if operations.include?(@operation)
+      
+    end
+
   end
 
 end
