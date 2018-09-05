3f6c3f8dd5c46bb6f8b81d65969e9966054122cf
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-05 17:51:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,6 +18,12 @@ class App < Sinatra::Base
 
   # Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a string the number of times given.
 
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    
+  end
+
   # Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
 
   # Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided. For example, going to /add/1/2 should render 3.
