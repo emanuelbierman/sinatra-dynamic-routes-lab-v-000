3bf4aff7c9fe9a8a5663216f72ec881b79545c37
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-05 17:53:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -26,6 +26,14 @@ class App < Sinatra::Base
 
   # Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+  end
+
   # Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided. For example, going to /add/1/2 should render 3.
 
 end
