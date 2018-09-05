c7892850eaa34274cd49132395de43f2918fd800
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-05 17:51:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ class App < Sinatra::Base
 
   # Create a dynamic route at get '/square/:number' that accepts a number and returns the square of that number. Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
 
+  get '/square/:number' do
+    @number = params[:number]
+    @square = @number.to_i ** 2
+    "#{@square.to_s}"
+  end
+
   # Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a string the number of times given.
 
   # Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
